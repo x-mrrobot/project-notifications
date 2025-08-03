d09dfb5e1dcc9e11b7b85a4045f383e8ac0a18b7
Mr Robot <mrrobotcodedev@gmail.com>
2025-08-03 13:45:20 -0300

    Create notifications.js

---
+++ notifications.js
@@ -0,0 +1,9 @@
+[
+  {
+    "id": 1,
+    "title": "Nova versão 2.1.0 disponível",
+    "body": "Baixe a nova versão com correções importantes",
+    "created_at": "2025-08-03T10:30:00Z",
+    "type": "info"
+  }
+]
